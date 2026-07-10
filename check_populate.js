f11a464dbf2e999e93c9260f27e3249edb7cf1e2
Giyuu57 <gouranshsattavan@gmail.com>
2026-07-10 15:16:26 +0530

    fixed

--- check_populate.js
+++ check_populate.js
@@ -4,7 +4,7 @@ import './models/medicine.js';
 import './models/user.js';
 import { Order } from './models/orders.js';
 
-const uri = "mongodb+srv://admin:admin@caresync.eu9ff.mongodb.net/CareSync?retryWrites=true&w=majority&appName=CareSync";
+const uri;
 
 async function main() {
   await mongoose.connect(uri);
