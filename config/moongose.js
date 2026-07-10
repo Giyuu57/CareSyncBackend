a62c901f92afa0a594187968c89337cb6a0e444a
Giyuu57 <gouranshsattavan@gmail.com>
2026-07-10 14:52:02 +0530

    changed 1 file with 1 insertion and 1 deletion

--- config/moongose.js
+++ config/moongose.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const run = async () => {
     try {
         await mongoose.connect(process.env.uri);
-        mongoose.connection.useDb('MediQ')
+        mongoose.connection.useDb('CareSync');
         console.log('Connected to database');
     } catch (error) {
         console.error('Not connected to database:',error);
